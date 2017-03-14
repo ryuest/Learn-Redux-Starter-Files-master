1fa805158b9ba0db649f6a7bbe2c26ec773dd540
Juri Boiko <jboiko@williamhill.co.uk>
2017-03-14 14:08:19 +0100

    fixed redux debug

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,21 +1,31 @@
 import { createStore, compose } from 'redux';
-import { syncHistoryWithStore } from 'react-router-redux';
+import { syncHistoryWithStore} from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
-// reducer
+// import the root reducer
 import rootReducer from './reducers/index';
 
 import comments from './data/comments';
 import posts from './data/posts';
 
-//create an object for the default data
+// create an object for the default data
 const defaultState = {
   posts,
   comments
 };
 
-const store = createStore(rootReducer, defaultState);
+const composeEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f );
+
+const store = createStore(rootReducer, defaultState, composeEnhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
-export default store; 
+if(module.hot) {
+  module.hot.accept('./reducers/',() => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+export default store;
